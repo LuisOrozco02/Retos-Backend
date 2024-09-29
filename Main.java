2ab38fd3363ff2665be720c37c2ee2a6dd087ab2
Luis Guillermo Orozco Hernandez <guillermo.hernandez9304@gmail.com>
2024-09-29 11:10:17 -0500

    new file:   Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void Main(String[] args) {
+
+        Persona persona = new Persona("Alejandra", 32, "Femenino", "Colombiana", 
+        "Negociadora");
+        
+
+    }
+}
